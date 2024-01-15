ce17d25b4488c048d5bd68056c0a0384abb23a33
weijia <2112025700@qq.com>
2024-01-15 15:42:45 +0800

    feat: 解决跨域问题

--- main.go
+++ main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"strings"
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+
 	"github.com/qweijiaq/webook/internal/web"
 )
 
 func main() {
 	server := gin.Default()
+
+	// 解决跨域问题
+	server.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:3000"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders: []string{"Content-Type", "Authorization"},
+		// ExposeHeaders:    []string{},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://localhost") {
+				// 开发环境
+				return true
+			}
+			// 生产环境
+			return strings.Contains(origin, "your domain name")
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 
 	u := web.NewUserHandler()
 	u.RegisterRoutes(server)
